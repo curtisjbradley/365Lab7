348710836bae87ccffd7d6aa9ff21d5d1b7e9197
juanito-lenteja <juan@fmalpartida.com>
2025-03-09 15:16:15 -0700

    initial set up for both files

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This Lab recreates an INN reservation manager. 
 
 # How To Run
-This project is written in Python 3, and it requires the user to build 
+This project is written in Python 3.11, and it requires the user to build 
 the app using the main.py file (subject to change).
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # we could make every request a function, which will hold default values set to equivalent of "Any"
 # Any could be represented as an empty string since it symbolizes no constraints at all; it could also be
 # AND TRUE within our WHERE statement.
+import mysql.connector
+
+
+def main():
+
+
+    return
+
+
+if __name__ == "__main__":
+    main()
