a9dfe75b5b1a1fdf3adc7badf3565c647c005ce0
juanito-lenteja <juan@fmalpartida.com>
2025-03-09 14:51:23 -0700

    added main.py file and jolted down some ideas.

---
+++ main.py
@@ -0,0 +1,3 @@
+# we could make every request a function, which will hold default values set to equivalent of "Any"
+# Any could be represented as an empty string since it symbolizes no constraints at all; it could also be
+# AND TRUE within our WHERE statement.
